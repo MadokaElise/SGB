54a8b44ecc5a43cd21da41b75e10b06ec68f5344
guillaumemontfort19 <guillaume.montfort19@gmail.com>
2020-02-28 11:35:07 +0100

    salut

---
+++ src/Personnage.java
@@ -0,0 +1,31 @@
+public class Personnage {
+	
+	
+	int mana;
+	int pvs;
+	int force;
+	int agilite;
+	
+	
+	
+	public Personnage(){
+		
+		mana=0;
+		pvs=0;
+		force=0;
+		agilite=0;
+	}
+
+	
+	public Personnage(int pmana, int ppvs, int pagilite, int pforce){
+		
+		mana=pmana;
+		pvs=ppvs;
+		force=pforce;
+		agilite=pagilite;
+		
+	}
+	
+	
+}
+
---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Personnage Fraise = new Personnage(10, 40, 10, 10);
+		System.out.println(Fraise.pvs);
+
+	}
+
+}
