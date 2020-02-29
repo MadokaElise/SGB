f684521ba2f79e92d0c32ac0e3703b7d281b0235
MadokaElise <elise.casaccia@gmail.com>
2020-02-29 16:01:45 +0100

    tableau pour map + affichage

--- src/Panneau.java
+++ src/Panneau.java
@@ -9,25 +9,27 @@ public class Panneau extends JPanel {
   public void paintComponent(Graphics g){
     try {
       int posX, posY ;
-      // 0 : herbe , 2 chemin vert, 1: chemin hor, 3: virage bas gacuhe
+      // 0 : herbe , 2 chemin vert, 1: chemin hor, 3: virage bas gacuhe, 4 virage bas droite, 5 , virage haut gauche , 6 virage haut droite
       int tab[][] = { {0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                       {1,1,1,1,1,1,3,0,0,0,0,0,0,0},
                       {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
                       {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
                       {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
                       {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
-                      {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
-                      {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
-                      {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
-                      {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
-                      {0,0,0,0,0,0,2,0,0,0,0,0,0,0},
-                      {0,0,0,0,0,0,2,0,0,0,0,0,0,0},};
+                      {0,0,0,0,0,0,6,1,1,1,1,1,3,0},
+                      {0,0,0,0,0,0,0,0,0,0,0,0,2,0},
+                      {0,0,0,0,0,0,0,0,4,1,1,1,5,0},
+                      {0,0,0,0,0,0,0,0,2,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,2,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,2,0,0,0,0,0},};
 
 
       Image chemin_vert = ImageIO.read(new File("chemin_verticale.png"));
       Image chemin_hor = ImageIO.read(new File("chemin_horizontale.png"));
       Image virage_bas_gauche = ImageIO.read(new File("virage_bas_gauche.png"));
       Image virage_bas_droite = ImageIO.read(new File("virage_bas_droite.png"));
+      Image virage_haut_gauche = ImageIO.read(new File("virage_haut_gauche.png"));
+      Image virage_haut_droite = ImageIO.read(new File("virage_haut_droite.png"));
       Image herbe = ImageIO.read(new File("herbe.png"));
       for (int y=0 ; y <=13 ; y ++){
         posX = y*70;
@@ -41,6 +43,15 @@ public class Panneau extends JPanel {
           }
           else if( tab[x][y] == 3){
             g.drawImage(virage_bas_gauche, posX , posY, this);
+          }
+          else if( tab[x][y] ==4 ) {
+            g.drawImage(virage_bas_droite, posX , posY, this);
+          }
+          else if( tab[x][y] == 5){
+            g.drawImage(virage_haut_gauche, posX , posY, this);
+          }
+          else if( tab[x][y] == 6){
+            g.drawImage(virage_haut_droite, posX , posY, this);
           }
           else if (tab[x][y] == 0){
             g.drawImage(herbe, posX , posY, this);
