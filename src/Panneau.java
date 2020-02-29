8d5b9a4a1a9c22e10b9f185b1daa1ead7c261f17
MadokaElise <elise.casaccia@gmail.com>
2020-02-29 14:11:11 +0100

    idea project + git ignore

--- src/Panneau.java
+++ src/Panneau.java
@@ -8,8 +8,15 @@ import javax.swing.JPanel;
 public class Panneau extends JPanel {
   public void paintComponent(Graphics g){
     try {
-      Image img = ImageIO.read(new File("fraise.jpg"));
-      g.drawImage(img, 0, 0, this);
+      Image img1 = ImageIO.read(new File("chemin_vertical.png"));
+
+      for (int pos=0 ; pos <=5 ; pos ++){
+        g.drawImage(img1, pos*70, 70, this);
+      }
+      int h =0;
+      Image img2 = ImageIO.read(new File("fraise_face.png"));
+      g.drawImage(img2, 0, h, this);
+      g.drawImage(img2, 0, 100, this);
       //Pour une image de fond
       //g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
     } catch (IOException e) {
